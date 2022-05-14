12aa384a82c95352e3e495d3797750015b908c8a
Nicholas Olgado <nicholas831@gmail.com>
2022-05-14 16:08:43 -0400

    convert root.py to object oriented import
    more as a library inclusion now

--- main.py
+++ main.py
@@ -3,3 +3,11 @@
 using https://youtu.be/-RdOwhmqP5s
 create a interactive visualization to show and manipulate the colored region
 '''
+
+import roots as r
+
+
+test = r.POLY([5, 4, 3, 2, 1, 10], n=50)
+testD = r.POLY(test.derivative, n=20)
+# print(test.roots)
+print(testD.roots)
--- roots.py
+++ roots.py
@@ -1,148 +1,106 @@
 '''FINIDNG ROOTS
 '''
-import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
 SIZE = 1024         # size of canvas
 DELTA = 1e-10       # when to stop calculating root
-N = 1000            # max iterations (avoiding endless loops)
+  
+class POLY:
+    def __init__(self, coefficients, lowerB=-10, upperB=10, n=30):
+        assert(len(coefficients) >= 1)
+        self.coefficients = coefficients
+        self.derivative = np.polyder(self.coefficients, 1)
+        self.roots = self.calc_roots(lowerB, upperB, n)
 
-def poly_name(coeff):
-    '''
-    poly_name: from the coefficients of the polynomial, 
-    return a string that is human readable of the polynomial
-    
-    :param coeff: array of polynomial coefficients in decending order (highest degree first!)
+    def calc_roots(self, lowerB, upperB, N):
+        '''
+        calc_roots: return the roots of the polynomial object
+        :param lowerB: lower bound for generating inital guesses
+        :param upperB: upper bound for generating inital guesses
+        :param N: sample size, N=sqrt(num_guesses)
+        :return: array of complex root values
+        '''
+        maxIter = 1000
+        sampleR = [np.random.uniform(lowerB, upperB) for x in range(N)]
+        sampleI = [np.random.uniform(lowerB, upperB) for x in range(N)]
 
-    :return: string of written out polynomial
-    '''
-    ans = ''
-    count = len(coeff)-1
-    for i in coeff:
-        if count == 0:
-            ans += str(i)
-            ans += '\n'
-        else:
-            ans += '{}x**'.format(i) + str(count)
-            ans += ' + '    
-        count -= 1
-    return ans
+        roots = []
 
-def plot_complex(roots, coeff):
-    '''
-    plot_complex: using the roots and coefficients of the polynomial, 
-    plot the roots using matplotlib
+        for r in sampleR:
+            for i in sampleI:
+                guess = r + i*1j
+                for n in range(maxIter):
+                    next_guess = guess - (self.eval(guess) / self.eval_derivative(guess))
+                    if abs(next_guess - guess) < DELTA:
+                        is_in = False
+                        for root in roots:
+                            if abs(next_guess - root) < DELTA:
+                                is_in = True
+                                break
+                        if not is_in:
+                            roots.append(next_guess)
+                        break
+                    guess = next_guess
+        return roots
 
-    :param roots: array of polynomial roots
-    :param coeff: array of polynomial coefficients in decending order (highest degree first!)
+    def eval(self, x):
+        '''
+        eval: to find the value of the polynomial with value x
+        :param x: value to evaluate polynomial
+        :return: polynomial evaluated with x
+        '''
+        ans = 0
+        count = 0
+        for i in reversed(self.coefficients):
+            ans += i*np.power(x, count)
+            count += 1
+        return ans
 
-    :return: matplotlib plot of roots from polynomial with labels
-    '''
-    # extract real part
-    x = [ele.real for ele in roots]
-    # extract imaginary part
-    y = [ele.imag for ele in roots]
-    
-    # plot the complex numbers
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    c = ax.scatter(x, y)
-    plt.ylabel('Imaginary')
-    plt.xlabel('Real')
-    plt.title("Complex Roots of Polynomial\n{}".format(poly_name(coeff)))
-    plt.grid(True)
-    plt.show()
+    def eval_derivative(self, x):
+        '''
+        eval_derivative: to find the value of the derivative with value x
+        :param x: value to evaluate polynomial
+        :return: derivative evaluated with x
+        '''
+        ans = 0
+        count = 0
+        for i in reversed(self.derivative):
+            ans += i*np.power(x, count)
+            count += 1
+        return ans    
 
-def calc_poly(coeff, x):
-    '''
-    calc_result to find the value of the polynomial with value x
+    def get_name(self):
+        '''
+        get_name: find the string formatted human readable polynomial of the object
+        :return: string with human readable polynomial
+        '''
+        ans = ''
+        count = len(self.coefficients)-1
+        for i in self.coefficients:
+            if bool(i):
+                if count == 0:
+                    ans += str(i)
+                    ans += '\n'
+                else:
+                    ans += '{}x**'.format(i) + str(count)
+                    ans += ' + '    
+                count -= 1
+            else:
+                count -= 1
+        return ans
 
-    :param coeff: array of polynomial coefficients in decending order (highest degree first!)
-    :param x: value to evaluate polynomial
-
-    :return: polynomial evaluated with x
-    '''
-    ans = 0
-    count = 0
-    for i in reversed(coeff):
-        ans += i*np.power(x, count)
-        count += 1
-    return ans
-
-def approx_root(coeff, deriv, lowerB=-10, upperB=10, sampleSize=25):
-    '''
-    approx_root: given the polynomial and bounds, approximate the complex
-    roots using sampling and newton's method
-
-    :param coeff: array of polynomial coefficients in decending order (highest degree first!)
-    :param deriv: array of polynomial derivative coefficients
-    :param lowerB: given lower bound of initial guess 
-    :param upperB: given upper bound of initial guess 
-    :param sampleSize: number of samples to be approximated (sampleSize * sampleSize is the total space)
-
-    :return: polynomial roots evaluated using Newton's method
-    '''
-    sampleR = [np.random.uniform(lowerB, upperB) for x in range(sampleSize)]
-    sampleI = [np.random.uniform(lowerB, upperB) for x in range(sampleSize)]
-
-    roots = []
-
-    for r in sampleR:
-        for i in sampleI:
-            guess = r + i*1j
-            for n in range(N):
-                next_guess = guess - (calc_poly(coeff, guess) / calc_poly(deriv, guess))
-                if abs(next_guess - guess) < DELTA:
-                    is_in = False
-                    for root in roots:
-                        if abs(next_guess - root) < DELTA:
-                            is_in = True
-                            break
-                    if not is_in:
-                        roots.append(next_guess)
-                    break
-                guess = next_guess
-    return roots
-
+#CLI HANDLING
 if __name__ == "__main__":
-    try:
-        arguments = sys.argv[1:]
-    except:
-        arguments = []
-
-    print(arguments)
-
-    try:
-        arg_list = arguments[0].split(',')
-        coeff = [int(x) for x in arg_list] 
-        deriv = np.polyder(coeff, 1)
-    except:
+    if len(sys.argv) > 1: 
+        #has args
+        args = [int(x) for x in sys.argv[1:]]
+        print("finding roots to polynomial {}...".format(args))
         try:
-            coeff = np.array(input("enter the values of the polynomial (seperated by spaces):\n").split(" "), dtype=np.int32)
-            deriv = np.polyder(coeff, 1)
+            f = POLY(args)
+            print("found polynomial {}".format(f.get_name()))
+            print("roots = {}".format(f.roots))
         except:
-            print("couldn't load coefficients..\n")
-            coeff = [np.round(np.random.rand()*100) for x in range(np.round(np.random.rand()*10))]
-            deriv = np.polyder(coeff, 1)
-    
-    print("found coefficients {}, with a derivative of {}".format(coeff, deriv))
-
-    try:
-        lb = int(arguments[1])
-        ub = int(arguments[2])
-    except:
-        print('no arguments for boundaries...\n')
-     
-    try:
-        ss = int(arguments[3])
-    except:
-        print('no arguments for sample size...\n')
-
-    try:
-        root = approx_root(coeff, deriv, lb, ub, ss)
-    except:
-        root = approx_root(coeff, deriv)
-
-    print("average root found at: {}\n\n".format(str(root)))
-    plot_complex(root, coeff)
+            print("couldn't find roots...")
+    else:
+        pass
