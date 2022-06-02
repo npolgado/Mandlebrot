14d43e15d4227e0e0125c533a2d688c1613c1d28
EricNicholls97 <67706297+EricNicholls97@users.noreply.github.com>
2022-06-01 22:57:24 -0700

    Update painter.py

--- painter.py
+++ painter.py
@@ -6,10 +6,12 @@ class Painter:
     def __init__(self, poly_roots=None):
         pygame.init()
 
+        self.i = 0
+
         pygame.display.set_caption('Fractals Babyy')
 
-        self.__resolution__ = 250
-        self.__scalar__ = 4
+        self.__resolution__ = 1000
+        self.__scalar__ = 1
         self.__size__ = self.__resolution__ * self.__scalar__
         self.__screen__ = pygame.display.set_mode((self.__size__, self.__size__))
 
@@ -38,24 +40,27 @@ class Painter:
         pygame.draw.line(self.__screen__, (0,0,0), (0, self.__size__/2), (self.__size__, self.__size__/2))  # horizontal line
 
     def draw_resolution(self, x, y, id):
+        if (self.i % 10000 == 0):
+            print(self.i)
+        self.i += 1
+        pass
 
-        id_real = round(np.real(id), 5)
-        id_imag = round(np.imag(id), 5)
-
-        id_rounded = id_real + id_imag * 1j
-
-        if id_rounded in self.__resolution_dict__.keys():
-            color = self.__resolution_dict__[id_rounded]
-        else:
-            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-            self.__resolution_dict__[id_rounded] = color
-
-        print(id_rounded, color)
-        # time.sleep(1)
-
-        self.__screen__.fill(color, ((x*self.__scalar__,y*self.__scalar__), (self.__scalar__, self.__scalar__)))
-
-        self.update()
+        # id_real = round(np.real(id), 5)
+        # id_imag = round(np.imag(id), 5)
+        #
+        # id_rounded = id_real + id_imag * 1j
+        #
+        # if id_rounded in self.__resolution_dict__.keys():
+        #     color = self.__resolution_dict__[id_rounded]
+        # else:
+        #     color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        #     self.__resolution_dict__[id_rounded] = color
+        #
+        # print(id_rounded, color)
+        # # time.sleep(1)
+        #
+        # self.__screen__.fill(color, ((x*self.__scalar__,y*self.__scalar__), (self.__scalar__, self.__scalar__)))
+        # self.update()
 
     def clear(self):
         self.__screen__.fill((255, 255, 255))
